82440343560d77eb7b867cd02f65cd6b6f1ce7ee
Zakaria Mansouri <zibanpirate@ZMBPL11.local>
2019-08-03 23:41:06 +0100

    added 'add' command

--- bin/add/index.js
+++ bin/add/index.js
@@ -1,2 +1,41 @@
-#! /usr/bin/env node
-console.log('add command');
+const arg = require("arg");
+
+module.exports = () => {
+  // choose what command:
+  const args = arg({});
+  if (args._.length <= 1) { console.log("missing command!"); return; }
+
+  switch (args._[1]) {
+    case "entry":
+      console.log("entry");
+      break;
+
+    case "config":
+      console.log("config");
+      break;
+
+    case "type":
+      console.log("type");
+      break;
+
+    case "scene":
+      console.log("scene");
+      break;
+
+    case "component":
+      console.log("component");
+      break;
+
+    case "lazy-action":
+      console.log("lazy-action");
+      break;
+
+    case "action":
+      console.log("action");
+      break;
+
+    default:
+      console.log("unknown command!");
+      break;
+  }
+};
--- bin/t9.js
+++ bin/t9.js
@@ -1,3 +1,30 @@
 #! /usr/bin/env node
-console.log('hello from t9')
-console.log('hello from t9')
+
+const chalk = require("chalk");
+const figlet = require("figlet");
+const arg = require("arg");
+
+// show script introduction
+console.log(chalk.green(figlet.textSync("template9")));
+
+// choose what command:
+const args = arg({});
+if (args._.length <= 0) { console.log("missing command!"); process.exit(); }
+
+switch (args._[0]) {
+  case "init":
+    console.log("init");
+    break;
+
+  case "upgrade":
+    console.log("upgrade");
+    break;
+
+  case "add":
+    require("./add")();
+    break;
+
+  default:
+    console.log("unknown command!");
+    break;
+}
