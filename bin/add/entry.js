5f2bbbedd4f59ae110972de905e776a504ceb553
Zakaria Mansouri <zibanpirate@ZMBPL11.local>
2019-08-04 20:27:32 +0100

    moved entry questions to config file

--- bin/add/entry.js
+++ bin/add/entry.js
@@ -1,28 +1,9 @@
 const inquirer = require("inquirer");
+const questions = require("../config");
 
 const askQuestions = () => {
-  const questions = [
-    {
-      name: "name",
-      type: "input",
-      message: "What is the name of the entry?",
-      validate: (answer) => {
-        if (!answer) return "please provide a name for the entry";
-        else return true;
-      },
-    },
-    {
-      name: "defaultLanguageCode",
-      type: "input",
-      message: "What is the default language code for this entry?",
-      default: "en",
-      validate: (answer) => {
-        if (!answer) return "please provide a the default language code for this entry";
-        else return true;
-      },
-    },
-  ];
-  return inquirer.prompt(questions);
+  const answers = await inquirer.prompt(questions.entryQuestions);
+  return answers;
 };
 
 module.exports = async () => {
