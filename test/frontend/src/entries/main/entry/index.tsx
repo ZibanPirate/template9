ee3d500d5a7466a377bafef0626740627f8f57b7
Zakaria Mansouri <zaqo.cs@outlook.com>
2019-08-13 10:14:19 +0100

    done with SCSS and localizing it

--- test/frontend/src/entries/main/entry/index.tsx
+++ test/frontend/src/entries/main/entry/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { render } from "react-dom";
+import "./style";
 
 document.title = "{|page title|}";
 
 render(
   (
     <>
-      <h1>{"{|Hello|} from main page"}</h1>
-      <h3>{"the language used is {|en|}"}</h3>
+      {/* <h1>{"{|Hello|} from main page"}</h1> */}
+      {/* <h3>{"the language used is {|en|}"}</h3> */}
     </>
   )
   ,
--- test/frontend/webpack.config.js
+++ test/frontend/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const StringReplacePlugin = require("string-replace-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // setting up project configs
 const t9config = require("./t9config.json");
@@ -12,6 +13,40 @@ const port = 3002;
 const entriesPathLength = (__dirname + "/src/entries/").length;
 
 const webpackConfigArray = [];
+// match replacement function
+const replacement = (language) => function (match) {
+  // we have: language, file path, placeholder, entry
+  const entry = this.resourcePath.substring(entriesPathLength, this.resourcePath.indexOf("/", entriesPathLength));
+  const placeholder = match.slice(2, -2);
+
+  console.log("\n MATCH: ", this.resourcePath);
+
+  // try to get from overwrites
+  const overwrites = require(`./src/entries/${entry}/entry/dictionary-overwrites.json`);
+  let value;
+  if (overwrites["*"] && (value = overwrites["*"][placeholder])) {
+    return value;
+  } else if (overwrites[language] && (value = overwrites[language][placeholder])) {
+    return value;
+  } else {
+    // then, try from this.resourcePath.json
+    const fileDictionary = require(path.join(path.dirname(this.resourcePath), "/dictionary.json"));
+    if (fileDictionary[language] && (value = fileDictionary[language][placeholder])) {
+      return value;
+    } else {
+      // and then try the fallbacks
+      const fallbacks = require(`./src/entries/${entry}/entry/dictionary-fallbacks.json`);
+      if (fallbacks["*"] && (value = fallbacks["*"][placeholder])) {
+        return value;
+      } else if (fallbacks[language] && (value = fallbacks[language][placeholder])) {
+        return value;
+      } else {
+        // finally if it's not found, set a placeholder value instead
+        return "PLACEHOLDER";
+      }
+    }
+  }
+};
 
 // pushWebpackConfig function
 const pushWebpackConfig = (language) => {
@@ -32,7 +67,7 @@ const pushWebpackConfig = (language) => {
     // https://webpack.js.org/configuration/entry-context/#dynamic-entry
     entry: () => t9config.entries.reduce((entries, entry) => ({
       ...entries,
-      [entry]: path.join(__dirname, "./src/entries/", entry, "/entry/index.tsx")
+      [entry]: path.join(__dirname, "./src/entries/", entry, "/entry/index.tsx"),
     }), {}),
     // https://webpack.js.org/configuration/mode/
     mode: isProduction ? "production" : (isDevelopment ? "development" : "none"),
@@ -44,37 +79,7 @@ const pushWebpackConfig = (language) => {
           loader: StringReplacePlugin.replace({
             replacements: [{
               pattern: /({\|)[A-Za-z0-9\s]+(\|})/ig,
-              replacement: function (match) {
-                // we have: language, file path, placeholder, entry
-                const entry = this.resourcePath.substring(entriesPathLength, this.resourcePath.indexOf("/", entriesPathLength));
-                const placeholder = match.slice(2, -2);
-
-                // try to get from overwrites
-                const overwrites = require(`./src/entries/${entry}/entry/dictionary-overwrites.json`);
-                let value;
-                if (overwrites["*"] && (value = overwrites["*"][placeholder])) {
-                  return value;
-                } else if (overwrites[language] && (value = overwrites[language][placeholder])) {
-                  return value;
-                } else {
-                  // then, try from this.resourcePath.json
-                  const fileDictionary = require(path.join(path.dirname(this.resourcePath), "/dictionary.json"));
-                  if (fileDictionary[language] && (value = fileDictionary[language][placeholder])) {
-                    return value;
-                  } else {
-                    // and then try the fallbacks
-                    const fallbacks = require(`./src/entries/${entry}/entry/dictionary-fallbacks.json`);
-                    if (fallbacks["*"] && (value = fallbacks["*"][placeholder])) {
-                      return value;
-                    } else if (fallbacks[language] && (value = fallbacks[language][placeholder])) {
-                      return value;
-                    } else {
-                      // finally if it's not found, set a placeholder value instead
-                      return "PLACEHOLDER";
-                    }
-                  }
-                }
-              },
+              replacement: replacement(language),
             }],
           }),
         },
@@ -82,6 +87,29 @@ const pushWebpackConfig = (language) => {
         { exclude: /node_modules/, loader: "babel-loader", test: /\.tsx?$/ },
         // https://webpack.js.org/loaders/source-map-loader/
         { enforce: "pre", loader: "source-map-loader", test: /\.js$/ },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: { hmr: isDevelopment },
+            },
+            {
+              loader: StringReplacePlugin.replace({
+                replacements: [{
+                  pattern: /({\|)[A-Za-z0-9\s]+(\|})/ig,
+                  replacement: replacement(language),
+                }],
+              }),
+            },
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: { plugins: () => [require("precss"), require("autoprefixer")] },
+            },
+            "sass-loader",
+          ],
+        },
       ],
     },
     // https://webpack.js.org/concepts/output/#multiple-entry-points
@@ -94,15 +122,20 @@ const pushWebpackConfig = (language) => {
     plugins: [
       // https://webpack.js.org/plugins/hot-module-replacement-plugin/
       isDevelopment ? new webpack.HotModuleReplacementPlugin() : null,
+      // https://github.com/webpack-contrib/mini-css-extract-plugin#advanced-configuration-example
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+        chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
+      }),
     ],
     // https://webpack.js.org/configuration/resolve/#resolveextensions
-    resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },
+    resolve: { extensions: [".ts", ".tsx", ".js", ".json", ".scss"] },
     // https://webpack.js.org/configuration/target/
     target: "web",
   });
 };
 
-// pushing configs
+// pushing configs for each language
 for (const language of t9config.languages) {
   // push a config version
   pushWebpackConfig(language);
